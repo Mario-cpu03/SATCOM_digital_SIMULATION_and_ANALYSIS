f175fe55d205f47d8fb82b343cd13aca08385354
Mario Pellegrino Ambrosone <marioambrosone03@gmail.com>
2025-06-30 11:43:12 +0200

    Structural dataset changes to improve coherence

--- src/MatLab/DataWriting.m
+++ src/MatLab/DataWriting.m
@@ -27,7 +27,8 @@ Resultcc = [BERcc, THRcc, PERcc, SNRcc, AWGNcc, ATMUPcc, ATMDWcc, TEMPcc, DENcc,
 
 %% Data Structure and init
 
-headers = {'meanBER', 'meanTRHOUGHPUT', 'meanPER', 'SNRpercomm', 'meanAWGN_POWER', 'meanUP_LOSS', 'meanDOWN_LOSS', 'meanTEMPERATURE', 'meanWV_DENSITY', 'meanUP_THERMAL_POWER', 'meanDW_THERMAL_POWER'};
+headerscc = {'meanBER_cc', 'meanTRHOUGHPUT_cc', 'meanPER_cc', 'SNRpercomm_cc', 'meanAWGN_POWER_cc', 'meanUP_LOSS_cc', 'meanDOWN_LOSS_cc', 'meanTEMPERATURE_cc', 'meanWV_DENSITY_cc', 'meanUP_THERMAL_POWER_cc', 'meanDW_THERMAL_POWER_cc'};
+headersnc = {'meanBER_nc', 'meanTRHOUGHPUT_nc', 'meanPER_nc', 'SNRpercomm_nc', 'meanAWGN_POWER_nc', 'meanUP_LOSS_nc', 'meanDOWN_LOSS_nc', 'meanTEMPERATURE_nc', 'meanWV_DENSITY_nc', 'meanUP_THERMAL_POWER_nc', 'meanDW_THERMAL_POWER_nc'};
 filepathnc = '../../data/DataSet_RAW_noCode.csv';
 filepathcc = '../../data/DataSet_RAW_ConvCode.csv';
 
@@ -35,7 +36,7 @@ filepathcc = '../../data/DataSet_RAW_ConvCode.csv';
 %% Writing first dataset
 
 fid = fopen(filepathnc, 'w');
-fprintf(fid, '%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n', headers{:});
+fprintf(fid, '%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n', headersnc{:});
 fclose(fid);
 dlmwrite(filepathnc, Resultnc, '-append');
 
@@ -43,7 +44,7 @@ dlmwrite(filepathnc, Resultnc, '-append');
 %% Writing second dataset
 
 fid = fopen(filepathcc, 'w');
-fprintf(fid, '%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n', headers{:});
+fprintf(fid, '%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n', headerscc{:});
 fclose(fid);
 dlmwrite(filepathcc, Resultcc, '-append');
 end
